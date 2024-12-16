c22b271db9cc89588ec6f91a6eac12d19f39a84f
Бочкарёв Егор ИТ-13 <131939223+Bochkarev-Egor-it1314@users.noreply.github.com>
2024-12-17 00:00:23 +0500

    Добавление в основной файл ф-ии play

--- Game_SelectNumber.cpp
+++ Game_SelectNumber.cpp
@@ -9,6 +9,18 @@ int RandNumber(int a, int b){
     return rand() % c + a;
 }
 
+int Play(int number, int a, int b){
+    int cnt = 0, x = MAXINT;
+    cout << "Теперь пытайтесь отгадать число в диапозоне от " << a << " до " << b << ":" << endl;
+    while (x != number){
+        cin >> x;
+        cnt++;
+        if (number < x) cout << "Число, которое я загадал, меньше " << x << endl;
+        if (number > x) cout << "Число, которое я загадал, больше " << x << endl;
+    }
+    return cnt;
+}
+
 int main()
 {
     
